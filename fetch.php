bfcb1cf91a109b341f5596106b20ec7e29e400d0
dratlas <ibis@connect.ie>
2019-02-03 19:32:19 +0000

    Add files via upload

--- fetch.php
+++ fetch.php
@@ -21,3 +21,10 @@ if(file_exists("current.json")){
 		file_put_contents("current.json",$contents,LOCK_EX);
 	}
 }
+else{
+	$url="https://www.water.ie/site-files/cms-templates/utils/proxy/index.xml?https://services2.arcgis.com/OqejhVam51LdtxGa/ArcGIS/rest/services/WaterAdvisoryCR021/FeatureServer/0/query?returnGeometry=true&where=STATUS!%3D%27Closed%27&outFields=*&orderByFields=STARTDATE%20DESC&outSR=4326&returnIdsOnly=false&f=pgeojson";
+	$contents=file_get_contents($url);
+	echo $contents;
+	$contents=preg_replace('/"crs"/','"version":"'.$date.'","crs"',$contents);
+	file_put_contents("current.json",$contents,LOCK_EX);
+}
